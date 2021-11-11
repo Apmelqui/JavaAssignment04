0ab3f715f97d37ad7aaf697b0ccad5aa821b1cbe
Adriano Melquiades <apmelqui@gmail.com>
2021-11-10 20:36:50 -0400

    Several changes - must implement comboBox

--- src/main/java/com/example/adrianomelquiades_comp228lab04/StudentInformationController.java
+++ src/main/java/com/example/adrianomelquiades_comp228lab04/StudentInformationController.java
@@ -3,15 +3,15 @@ package com.example.adrianomelquiades_comp228lab04;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import static javafx.application.Application.launch;
 
 public class StudentInformationController implements Initializable {
-    @FXML private TextArea StudentInformationLabel;
+    @FXML private TextArea taStudentInformation;
     @FXML private RadioButton computerScienceRadioButton;
     @FXML private RadioButton businessRadioButton;
     @FXML private TextField tfName;
@@ -23,51 +23,85 @@ public class StudentInformationController implements Initializable {
     @FXML private TextField tfEmail;
     @FXML private ComboBox cbCourses;
     @FXML private TextArea taCourses;
-    @FXML private CheckBox studentConcil;
+    @FXML private CheckBox studentCouncil;
     @FXML private CheckBox volunteerWork;
 
 
     String program = "The program is: ";
+    String checkBoxValue = "";
+    String checkBoxCouncil = "";
+    String checkBoxVolunteer = "";
+    ArrayList<String> selectedCourses = new ArrayList<String>();
+
 
 
     public void displayInformationButtonClicked() {
-        System.out.println("Button clicked");
-
 
         //Get checkBox value
-        String checkBoxValue = "";
-        if (studentConcil.isSelected()) {
-            checkBoxValue = "Student Concil";
-        } else if (volunteerWork.isSelected()) {
-            checkBoxValue = "Volunteer Work";
+        if (studentCouncil.isSelected()) {
+            checkBoxCouncil = studentCouncil.getText();
+        } if (volunteerWork.isSelected()) {
+            checkBoxVolunteer = volunteerWork.getText();
         }
 
 
         //Printing the student information:
-        this.StudentInformationLabel.setText(tfName.getText() + ", " + tfAddress.getText() + ", " + tfProvince.getText() + ", " +
+
+        displayStudentInfo();
+
+
+    }
+
+    public void displayStudentInfo() {
+
+        this.taStudentInformation.setText(tfName.getText() + ", " + tfAddress.getText() + ", " + tfProvince.getText() + ", " +
                 tfCity.getText() + ", " + tfPostalCode.getText() + ", " + tfPhoneNumber.getText() + ", " + tfEmail.getText() +
-                "\n" + program + "\nCourses: " + taCourses.getText().toString() + checkBoxValue);
-
-
-
-
-        System.out.println(program);
+                "\n" + program + "\nCourses: " + taCourses.getText().toString() + "\n" + checkBoxCouncil + "\n" + checkBoxVolunteer);
     }
 
     public void clearButtonClicked() {
-        StudentInformationLabel.clear();
+        taStudentInformation.clear();
         tfName.clear();
         tfAddress.clear();
+        tfProvince.clear();
+        tfCity.clear();
+        tfPostalCode.clear();
+        tfPhoneNumber.clear();
+        tfEmail.clear();
         taCourses.clear();
         computerScienceRadioButton.setSelected(false);
         businessRadioButton.setSelected(false);
         cbCourses.getItems().clear();
-        studentConcil.setSelected(false);
+        studentCouncil.setSelected(false);
         volunteerWork.setSelected(false);
     }
 
+
+
+
     public void loadComboBox() {
         //ComboBox
+        if (computerScienceRadioButton.isSelected()) {
+            cbCourses.getItems().clear();
+            cbCourses.getItems().addAll("Java", "Python", "C#", "JavaScript");
+            taCourses.clear();
+        } if (businessRadioButton.isSelected()) {
+            cbCourses.getItems().clear();
+            cbCourses.getItems().addAll("Financial Math", "Social Analysis", "Economy", "Stock analysis");
+            taCourses.clear();
+        }
+    }
+
+
+
+
+    //
+    //System.out.println(selectedCourses);
+
+//    taCourses.appendText(selectedCourses);
+
+
+        /*
         System.out.println("load Combobox method");
         cbCourses.getItems().clear();
 
@@ -87,13 +121,44 @@ public class StudentInformationController implements Initializable {
             program += "Business";
             this.taCourses.setText(cbCourses.getValue().toString());
         }
+
+
+         */
+
+
+
+
+    public void populateComboBox() {
+        String value = cbCourses.getValue().toString();
+        selectedCourses.add(value);
     }
+
+    public void displayComboBoxValuesOnTextAreaCourses(ArrayList<String> selectedCourses) {
+        for (int i = 0; i < selectedCourses.size(); i++) {
+            taCourses.setText(selectedCourses.get(i));
+        }
+    }
+
+    /*
+    delete
+    //Populate comboBox:
+        cbCourses.setOnAction((event) -> {
+
+
+        selectedCourses += cbCourses.getValue().toString();
+        System.out.println(selectedCourses);
+        this.taCourses.setText(selectedCourses);
+
+
+
+    });
+    delete
+     */
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("initialize method");
-        cbCourses.getItems().addAll("Java", "Python", "C#", "JavaScript");
+        loadComboBox();
 
     }
 }
